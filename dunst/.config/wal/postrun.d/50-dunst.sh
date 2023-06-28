f6f46321e54b06cd8622535f1801e3b3fb2aa1ff
nativerv <nativerv@protonmail.com>
2023-06-28 19:55:18 +0300

    feat(dunst): restart on `wal` postrun, use pipewire

--- dunst/.config/wal/postrun.d/50-dunst.sh
+++ dunst/.config/wal/postrun.d/50-dunst.sh
@@ -8,5 +8,11 @@
 cp -f "${XDG_CACHE_HOME}/wal/dunstrc" "${XDG_CONFIG_HOME}/dunst/dunstrc.d/50-wal.conf"
 
 # Restart dunst -- if using systemd
-command -v systemctl >/dev/null 2>&1 && \
-  systemctl --user restart "dunst@${DISPLAY#:*}.service"
+# FIXME: dunst user service is activating on the dbus interface (not display-aware)
+# command -v systemctl >/dev/null 2>&1 && \
+#   systemctl --user restart "dunst@${DISPLAY#:*}.service"
+(
+  sleep 2 &&
+    command -v 'systemctl' >/dev/null 2>&1 &&
+    systemctl --user restart "dunst.service"
+) &
