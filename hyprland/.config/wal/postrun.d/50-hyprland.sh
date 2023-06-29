d47c625974a4fb9e7b33380e096799cdc7c98854
nativerv <nativerv@protonmail.com>
2023-06-30 01:02:51 +0300

    feat(hyprland): use `wal` file from `XDG_CACHE_HOME`

--- hyprland/.config/wal/postrun.d/50-hyprland.sh
+++ hyprland/.config/wal/postrun.d/50-hyprland.sh
@@ -1,5 +1,2 @@
 # Source wal colors to Hyprland
 hyprctl keyword source "${XDG_CACHE_HOME}/wal/hyprland.conf"
-
-# Copy the file anyway because hyprland needs something at reload/startup
-cp -f "${XDG_CACHE_HOME}/wal/hyprland.conf" "${XDG_CONFIG_HOME}/hypr/colors.conf"
