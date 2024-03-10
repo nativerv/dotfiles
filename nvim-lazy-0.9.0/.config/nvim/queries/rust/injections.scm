b6ac3934585a4186022f6686d7fec138a6664892
nativerv <nativerv@protonmail.com>
2024-03-11 01:33:04 +0300

    docs(nvim): rust doc comment query

--- nvim-lazy-0.9.0/.config/nvim/queries/rust/injections.scm
+++ nvim-lazy-0.9.0/.config/nvim/queries/rust/injections.scm
@@ -5,9 +5,13 @@
   (_) @injection.content
   (line_comment) @_last 
 
-  (#match? @_last "^/// ```$")
-  (#offset! @injection.content 0 4 0 4)
-  (#gsub! @injection.language "^/// ```(%w+)%s*$" "%1")
-  (#gsub! @injection.language "^/// ```$" "rust")
-  (#set! injection.combined)
+  (#gsub! @injection.language "^/// ```(%w+)%s*$" "%1") ; match opening - extract language tag
+  (#gsub! @injection.language "^/// ```$" "rust") ; match opening - no language tag, default to rust
+  (#match? @_last "^/// ```$") ; match closing
+  (#offset! @injection.content 0 4 0 4) ; skip '/// ' on both start and end line i think
+
+  ; tell nvim to use extracted (substituted) tag as if it was name of the
+  ; capture which will then tell it what to inject 
+  ; more on how that works: `:h treesitter-language-injections`
+  (#set! injection.combined) 
 )
